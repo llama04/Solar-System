7c8d3f034dccbea9aab23d5a5054496b389ba88c
llama04 <lacuna64469@gmail.com>
2025-06-25 04:53:18 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,70 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
+import { useFrame } from "@react-three/fiber";
+import { useThree } from '@react-three/fiber';
 import * as THREE from 'three';
 
-const random = (a, b) => a + Math.random() * b;
-const randomInt = (a, b) => Math.floor(random(a, b));
-const randomColor = () =>
-  `rgb(${randomInt(80, 50)}, ${randomInt(80, 50)}, ${randomInt(80, 50)})`;
-const planetData = [];
-const totalPlanets = 6;
-for (let i = 0; i < totalPlanets; i++) {
-  planetData.push({
-    id: i,
-    color: randomColor(),
-    xRadius: (i + 1.5) * 4,
-    zRadius: (i + 1.5) * 2,
-    size: random(0.5, 1),
-  });
-}
+const response = await fetch('/Solar-System/src/planets.json');
+const planetData = await response.json();
+const planets = [];
 
 export default function App() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-    <Canvas camera={{ position: [0, 20, 25], fov: 45 }}>
+    <Canvas camera={{ position: [130,90,130], fov: 75 }}>
+      <color attach="background" args={["grey"]} />
       <Sun />
-      {planetData.map((planet) => (
+      <AsteroidBelt />
+      {planetData.planets.map((planet) => (
         <Planet planet={planet} key={planet.id} />
       ))}
+      <SaturnRing />
       <Lights />
       <OrbitControls />
     </Canvas>
-    </div>
+    </div> 
   );
 }
+
 function Sun() {
   return (
     <mesh>
-      <sphereGeometry args={[2.5, 32, 32]} />
+      <sphereGeometry args={[15, 32, 32]} />
       <meshStandardMaterial color="#E1DC59" />
     </mesh>
   );
 }
-function Planet({ planet: { color, xRadius, zRadius, size } }) {
+
+function Planet({ planet: { id,name,color,velocity, xOrbitalRadius, zOrbitalRadius, radius } }) {
+  const planet = React.useRef();
+  planets[id] = planet;
+  useFrame(({ clock }) => {
+      const index = clock.elapsedTime % 360;
+      const angle = (index / 360) * velocity * Math.PI;
+      planet.current.position.set(xOrbitalRadius * Math.cos(angle),0,zOrbitalRadius * Math.sin(angle));
+  })
   return (
     <>
-      <mesh position={[xRadius, 0, 0]}>
-        <sphereGeometry args={[size, 32, 32]} />
+      <mesh ref = {planet} name={name} position={[xOrbitalRadius, 0, 0]}>
+        <sphereGeometry args={[radius,32,32]}/>
         <meshStandardMaterial color={color} />
       </mesh>
-      <Ecliptic xRadius={xRadius} zRadius={zRadius} />
+      <Ecliptic xRadius={xOrbitalRadius} zRadius={zOrbitalRadius} />
     </>
   );
 }
+
 function Lights() {
   return (
     <>
-      <ambientLight />
+      <ambientLight intensity= {5}/>
       <pointLight position={[0, 0, 0]} />
     </>
   );
 }
 
-function Ecliptic({ xRadius = 1, zRadius = 1 }) {
+function Ecliptic({ xRadius, zRadius }) {
   const points = [];
   for (let index = 0; index < 64; index++) {
     const angle = (index / 64) * 2 * Math.PI;
@@ -75,5 +78,37 @@ function Ecliptic({ xRadius = 1, zRadius = 1 }) {
     <line geometry={lineGeometry}>
       <lineBasicMaterial attach="material" color="#BFBBDA" linewidth={5} />
     </line>
+  );
+}
+
+function AsteroidBelt(){
+  return(
+    <mesh rotation = {[Math.PI/2,0,0]}>
+      <torusGeometry args={[70,5,2]} />
+      <meshStandardMaterial color="#FFF" />
+    </mesh>
+  )
+}
+
+function SaturnRing(){
+  const ring = React.useRef();
+  useFrame(({ clock }) => {
+    ring.current.position.set(planets[5].current.position.x,0,planets[5].current.position.z);
+    console.log(planets[5].current.position);
+  })
+  return (
+    <mesh ref = {ring} position={[80,0,0]} rotation={[Math.PI/2,0,0]}>
+      <torusGeometry args={[4, 0.75, 2.5]} />
+      <meshStandardMaterial color="#eddbad" />
+    </mesh>
+  );
+}
+
+function Moon(){
+  return (
+    <mesh>
+      <sphereGeometry args={[15, 32, 32]} />
+      <meshStandardMaterial color="#E1DC59" />
+    </mesh>
   );
 }
