6d1237ba53eb6ee3a612fc38e2713549d01ef1f4
llama04 <lacuna64469@gmail.com>
2025-06-29 02:50:51 +0100

    added camera options and playback control

--- src/App.jsx
+++ src/App.jsx
@@ -8,33 +8,104 @@ import mercuryTexture from "./2k_mercury.jpg";import venusTexture from "./2k_ven
 
 const planets = [];
 const planetTextures = [mercuryTexture,venusTexture,earthTexture,marsTexture,jupiterTexture,saturnTexture,uranusTexture,neptuneTexture,plutoTexture];
-
-console.log(window.location.pathname);
+var trackingPlanet;
+var timeSpeed = 1;
 
 export default function App() {
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-    <Canvas camera={{ position: [120,60,120], fov: 80 }}>
-      <Environment files={spaceTexture} background backgroundBlurriness={0.02}/>
-      <Sun />
-      <AsteroidBelt />
-      {planetData.planets.map((planet) => (
-        <Planet planet={planet} key={planet.id} />
-      ))}
-      <SaturnRing />
-      <UranusRing />
-      <Moon />
-      <Lights />
-      <OrbitControls />
-    </Canvas>
+    <div >
+      <div className="overlay1">
+        <button type="button" onMouseDown={function(){trackingPlanet = planets[9]}}>Sun</button>
+        <div className="divider"></div>
+        <button type="button" onMouseDown={function(){trackingPlanet = planets[0]}}>Mercury</button>
+        <div className="divider"></div>
+        <button type="button" onMouseDown={function(){trackingPlanet = planets[1]}}>Venus</button>
+        <div className="divider"></div>
+        <button type="button" onMouseDown={function(){trackingPlanet = planets[2]}}>Earth</button>
+        <div className="divider"></div>
+        <button type="button" onMouseDown={function(){trackingPlanet = planets[10]}}>Moon</button>
+        <div className="divider"></div>
+        <button type="button" onMouseDown={function(){trackingPlanet = planets[3]}}>Mars</button>
+        <div className="divider"></div>
+        <button type="button" onMouseDown={function(){trackingPlanet = planets[4]}}>Jupiter</button>
+        <div className="divider"></div>
+        <button type="button" onMouseDown={function(){trackingPlanet = planets[5]}}>Saturn</button>
+        <div className="divider"></div>
+        <button type="button" onMouseDown={function(){trackingPlanet = planets[6]}}>Uranus</button>
+        <div className="divider"></div>
+        <button type="button" onMouseDown={function(){trackingPlanet = planets[7]}}>Neptune</button>
+        <div className="divider"></div>
+        <button type="button" onMouseDown={function(){trackingPlanet = planets[8]}}>Pluto</button>        
+      </div>
+      <div className="overlay2">
+        <button type="button" onMouseDown={function(){SpeedDown()}}>&lt;&lt;</button>
+        <div className="divider"></div>
+        <input type="text" id="speedDisplay" placeholder = "1x"></input>
+        <div className="divider"></div>
+        <button type="button" onMouseDown={function(){SpeedUp()}}>&gt;&gt;</button>
+      </div>
+      <div style={{ width: "100vw", height: "100vh" }}>
+      <Canvas camera={{ position: [120,60,120], fov: 80}}>
+        <SetButton/>
+        <Environment files={spaceTexture} background backgroundBlurriness={0.02}/>
+        <PlanetTracking />
+        <Sun />
+        <AsteroidBelt />
+        {planetData.planets.map((planet) => (
+          <Planet planet={planet} key={planet.id} />
+        ))}
+        <SaturnRing />
+        <UranusRing />
+        <Moon />
+        <Lights />
+      </Canvas>
+      </div>
     </div> 
   );
+}
+function SetButton(){
+  const input = document.getElementById("speedDisplay");
+  input.addEventListener("keypress", function(event){
+    if(event.key === "Enter"){
+      event.preventDefault();
+      if(input.value[input.value.length-1] == "x"){
+        timeSpeed = parseInt(input.value.substr(0,input.value.length-1));
+      }else{
+        timeSpeed = parseInt(input.value);
+      }
+      input.value = timeSpeed+"x";
+    }
+  })
+}
+function SpeedDown(){
+  if((timeSpeed /2) < 0.25){
+    timeSpeed = 0.25;
+  } else{
+    timeSpeed /= 2;
+  }
+  document.getElementById("speedDisplay").placeholder = timeSpeed+"x";
+}
+function SpeedUp(){
+  timeSpeed *=2;
+  document.getElementById("speedDisplay").placeholder = timeSpeed+"x";
+}
+
+function PlanetTracking(){
+  const tracking = React.useRef();
+  useFrame(({}) =>{
+    tracking.current.target = trackingPlanet.current.position;    
+  })
+  return(
+    <OrbitControls ref={tracking} />
+  )
 }
 
 function Sun() {
   const sun = React.useRef();
+  planets[9] = sun;
+  trackingPlanet = sun;
   useFrame(({clock}) => {
-    const index = clock.elapsedTime;
+    const index = clock.elapsedTime * timeSpeed;
     sun.current.rotation.y = index/20;
   })
   return (
@@ -49,8 +120,8 @@ function Planet({ planet: { id,name,velocity,rotation, xOrbitalRadius, zOrbitalR
   const planet = React.useRef();
   planets[id] = planet;
   useFrame(({ clock }) => {
-      const index = clock.elapsedTime;
-      planet.current.rotation.y = 15*index/rotation;
+      const index = clock.elapsedTime * timeSpeed;
+      planet.current.rotation.y = 15*index/rotation ;
       const angle = (index / 360) * velocity * Math.PI;
       planet.current.position.set(xOrbitalRadius * Math.cos(angle),0,zOrbitalRadius * Math.sin(angle));
   })
@@ -67,8 +138,8 @@ function Planet({ planet: { id,name,velocity,rotation, xOrbitalRadius, zOrbitalR
 
 function Ecliptic({ xRadius, zRadius }) {
   const points = [];
-  for (let index = 0; index < 64; index++) {
-    const angle = (index / 64) * 2 * Math.PI;
+  for (let index = 0; index < 360; index++) {
+    const angle = (index / 360) * 2 * Math.PI;
     const x = xRadius * Math.cos(angle);
     const z = zRadius * Math.sin(angle);
     points.push(new THREE.Vector3(x, 0, z));
@@ -77,7 +148,7 @@ function Ecliptic({ xRadius, zRadius }) {
   const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
   return (
     <line geometry={lineGeometry}>
-      <lineBasicMaterial attach="material" color="#BFBBDA" linewidth={5} />
+      <lineBasicMaterial attach="material" color="#BFBBDA" linewidth={0.5} />
     </line>
   );
 }
@@ -85,11 +156,11 @@ function Ecliptic({ xRadius, zRadius }) {
 function AsteroidBelt(){
   const belt = React.useRef();
   useFrame(({ clock }) => {
-    belt.current.rotation.z = clock.elapsedTime*2;
+    belt.current.rotation.z = clock.elapsedTime/2 * timeSpeed;
   })
   return(
     <mesh ref = {belt} rotation = {[Math.PI/2,0,0]}>
-      <torusGeometry args={[70,5,2]} />
+      <torusGeometry args={[70,5,2,90]} />
       <meshStandardMaterial map={useTexture(sRingTexture)}/>
     </mesh>
   )
@@ -99,9 +170,9 @@ function SaturnRing(){
   const ring = React.useRef();
   useFrame(({ clock }) => {
     ring.current.position.set(planets[5].current.position.x,0,planets[5].current.position.z);
-    ring.current.rotation.x = Math.PI/2+ Math.sin(clock.elapsedTime)/4;
-    ring.current.rotation.y = Math.cos(clock.elapsedTime)/4;
-    ring.current.rotation.z = clock.elapsedTime;
+    ring.current.rotation.x = Math.PI/2+ Math.sin(clock.elapsedTime)/4 * timeSpeed;
+    ring.current.rotation.y = Math.cos(clock.elapsedTime)/4 * timeSpeed;
+    ring.current.rotation.z = clock.elapsedTime * timeSpeed;
 
   })
   return (
@@ -126,12 +197,13 @@ function UranusRing(){
 
 function Moon(){
   const moon = React.useRef();
+  planets[10] = moon;
   useFrame(({ clock }) => {
     const index = clock.elapsedTime;
     moon.current.rotation.y = 15*index/655;
     const xEarthPos = planets[2].current.position.x;
     const zEarthPos = planets[2].current.position.z;
-    const angle = (index / 360) * 398 * Math.PI;
+    const angle = (index / 360) * 398 * Math.PI * timeSpeed;
     moon.current.position.set((3 * Math.cos(angle))+xEarthPos,0.5*Math.sin(angle),(3 * Math.sin(angle))+zEarthPos);
   })
   return (
@@ -141,6 +213,8 @@ function Moon(){
     </mesh>
   );
 }
+
+
 
 function Lights() {
   return (
